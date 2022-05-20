8677d5a4a571710fb4ab7536cd32f2fff872782c
SimbaWANGU <simbawangulu@gmail.com>
2022-05-20 11:16:40 +0300

    Fix linters

--- src/modules/commentCounter.test.js
+++ src/modules/commentCounter.test.js
@@ -3,7 +3,7 @@ import commentCounter from './commentCounter.js';
 describe('Pokemon counter', () => {
   test('Count number of pokemon in display', () => {
     const testComments = [
-      {}, {}, {}
+      {}, {}, {},
     ];
     expect(commentCounter(testComments)).toBe(`comments(${testComments.length})`);
   });
